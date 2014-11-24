6cddb861120a0293270594e85039e6b07f859baf
edsilv <e.silverton@gmail.com>
2014-11-24 17:16:51 +0000

    build

--- dist/PixelPalette.d.ts
+++ dist/PixelPalette.d.ts
@@ -1,7 +1,6 @@
 declare class PixelPalette {
-    private context;
     private imgPath;
-    constructor(context: CanvasRenderingContext2D, imgPath: string);
+    constructor(imgPath: string);
     Load(callback: (palette: string[]) => void): void;
 }
 export = PixelPalette;
