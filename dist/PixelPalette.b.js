348e4c498c6b06d6858f60fa42ce9a1ef1e99f26
edsilv <e.silverton@gmail.com>
2014-11-24 17:19:22 +0000

    build

--- dist/PixelPalette.b.js
+++ dist/PixelPalette.b.js
@@ -540,18 +540,18 @@ if (typeof define === 'function' && define.amd) {
 }
 !function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.PixelPalette=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 var PixelPalette = function () {
-        function PixelPalette(context, imgPath) {
-            this.context = context;
+        function PixelPalette(imgPath) {
             this.imgPath = imgPath;
         }
         PixelPalette.prototype.Load = function (callback) {
-            var _this = this;
             var loader = new PxLoader();
             var img = loader.addImage(this.imgPath);
+            var canvas = document.createElement('canvas');
+            var context = canvas.getContext('2d');
             loader.addCompletionListener(function () {
                 var len = img.width;
-                _this.context.drawImage(img, 0, 0, len, 1);
-                var imgd = _this.context.getImageData(0, 0, len, 1);
+                context.drawImage(img, 0, 0, len, 1);
+                var imgd = context.getImageData(0, 0, len, 1);
                 var pal = imgd.data;
                 var palette = [];
                 for (var i = 0; i < len * 4; i += 4) {
