948279af460be27959f4cbaf309bd6a9a3319f6f
Whitevinyl <neverest_songs@yahoo.co.uk>
2015-08-16 10:04:55 +0100

    Returns array of RGBA objects instead of flattened strings

--- src/PixelPalette.ts
+++ src/PixelPalette.ts
@@ -1,4 +1,4 @@
-
+import RGBA = require("./RGBA");
 declare var PxLoader;
 
 // note: if you want to make this an amd module, you have to export it.
@@ -10,7 +10,7 @@ class PixelPalette {
 
     }
 
-    public Load(callback: (palette: string[]) => void): void {
+    public Load(callback: (palette: RGBA[]) => void): void {
         var loader = new PxLoader(); //// Use PX Loader to handle image load
         var img = loader.addImage(this.imgPath);
 
@@ -24,10 +24,10 @@ class PixelPalette {
             var imgd = context.getImageData(0, 0, len, 1); //// get the image data
             var pal = imgd.data;
 
-            var palette: string[] = []; //// Loop over each pixel and add the color to an array.
+            var palette: RGBA[] = []; //// Loop over each pixel and add the color to an array.
 
             for (var i = 0; i < (len*4); i += 4) {
-                palette[i/4] = "rgba("+pal[i]+", "+pal[i+1]+", "+pal[i+2]+", 1)";
+                palette[i/4] = new RGBA(pal[i],pal[i+1],pal[i+2],pal[i+3]);
             }
 
             callback(palette);
---
+++ src/RGBA.ts
@@ -0,0 +1,21 @@
+/**
+ * Created by luketwyman on 16/08/2015.
+ */
+
+class RGBA {
+
+    public R: number;
+    public G: number;
+    public B: number;
+    public A: number;
+
+    constructor(r: number, g: number, b: number, a: number) {
+        this.R = r;
+        this.G = g;
+        this.B = b;
+        this.A = a;
+    }
+
+}
+
+export = RGBA;
